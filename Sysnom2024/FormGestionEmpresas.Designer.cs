07464d31a177a4fa7ca064bc9f78925d1d9848f2
Justin131 <justin235233@gmail.com>
2024-07-31 20:11:15 -0400

    administraciones

--- Sysnom2024/FormGestionEmpresas.Designer.cs
+++ Sysnom2024/FormGestionEmpresas.Designer.cs
@@ -28,209 +28,208 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button5 = new Button();
-            button4 = new Button();
-            button3 = new Button();
-            button2 = new Button();
-            button1 = new Button();
-            dataGridView1 = new DataGridView();
-            label8 = new Label();
-            textBox4 = new TextBox();
-            textBox3 = new TextBox();
-            textBox2 = new TextBox();
-            textBox1 = new TextBox();
-            label7 = new Label();
-            label6 = new Label();
-            label5 = new Label();
-            label4 = new Label();
-            label3 = new Label();
-            label2 = new Label();
-            label1 = new Label();
-            textBox5 = new TextBox();
-            textBox6 = new TextBox();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            btnSalir = new Button();
+            btnEliminar = new Button();
+            btnActualizar = new Button();
+            btnGuardar = new Button();
+            btnNuevo = new Button();
+            dataGridViewEmpresas = new DataGridView();
+            ListaDeEmpresas = new Label();
+            txtSitioWeb = new TextBox();
+            txtRNC = new TextBox();
+            txtNombre = new TextBox();
+            txtIDEmpresa = new TextBox();
+            Direccion = new Label();
+            SitioWeb = new Label();
+            Propietario = new Label();
+            RNC = new Label();
+            Nombre = new Label();
+            IDEmpresa = new Label();
+            GestionDeEmpresas = new Label();
+            txtDireccion = new TextBox();
+            txtPropietario = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewEmpresas).BeginInit();
             SuspendLayout();
             // 
-            // button5
+            // btnSalir
             // 
-            button5.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            button5.ForeColor = Color.FromArgb(192, 0, 0);
-            button5.Location = new Point(694, 16);
-            button5.Name = "button5";
-            button5.Size = new Size(93, 27);
-            button5.TabIndex = 59;
-            button5.Text = "Salir";
-            button5.UseVisualStyleBackColor = true;
-            button5.Click += button5_Click;
+            btnSalir.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btnSalir.ForeColor = Color.FromArgb(192, 0, 0);
+            btnSalir.Location = new Point(694, 16);
+            btnSalir.Name = "btnSalir";
+            btnSalir.Size = new Size(93, 27);
+            btnSalir.TabIndex = 59;
+            btnSalir.Text = "Salir";
+            btnSalir.UseVisualStyleBackColor = true;
+            btnSalir.Click += btnSalir_Click;
             // 
-            // button4
+            // btnEliminar
             // 
-            button4.Location = new Point(686, 376);
-            button4.Name = "button4";
-            button4.Size = new Size(101, 42);
-            button4.TabIndex = 58;
-            button4.Text = "Eliminar";
-            button4.UseVisualStyleBackColor = true;
+            btnEliminar.Location = new Point(686, 376);
+            btnEliminar.Name = "btnEliminar";
+            btnEliminar.Size = new Size(101, 42);
+            btnEliminar.TabIndex = 58;
+            btnEliminar.Text = "Eliminar";
+            btnEliminar.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnActualizar
             // 
-            button3.Location = new Point(686, 308);
-            button3.Name = "button3";
-            button3.Size = new Size(101, 42);
-            button3.TabIndex = 57;
-            button3.Text = "Actualizar";
-            button3.UseVisualStyleBackColor = true;
+            btnActualizar.Location = new Point(686, 308);
+            btnActualizar.Name = "btnActualizar";
+            btnActualizar.Size = new Size(101, 42);
+            btnActualizar.TabIndex = 57;
+            btnActualizar.Text = "Actualizar";
+            btnActualizar.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnGuardar
             // 
-            button2.Location = new Point(686, 237);
-            button2.Name = "button2";
-            button2.Size = new Size(101, 42);
-            button2.TabIndex = 56;
-            button2.Text = "Guardar";
-            button2.UseVisualStyleBackColor = true;
+            btnGuardar.Location = new Point(686, 237);
+            btnGuardar.Name = "btnGuardar";
+            btnGuardar.Size = new Size(101, 42);
+            btnGuardar.TabIndex = 56;
+            btnGuardar.Text = "Guardar";
+            btnGuardar.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // btnNuevo
             // 
-            button1.Location = new Point(686, 153);
-            button1.Name = "button1";
-            button1.Size = new Size(101, 42);
-            button1.TabIndex = 55;
-            button1.Text = "Nuevo";
-            button1.UseVisualStyleBackColor = true;
+            btnNuevo.Location = new Point(686, 153);
+            btnNuevo.Name = "btnNuevo";
+            btnNuevo.Size = new Size(101, 42);
+            btnNuevo.TabIndex = 55;
+            btnNuevo.Text = "Nuevo";
+            btnNuevo.UseVisualStyleBackColor = true;
             // 
-            // dataGridView1
+            // dataGridViewEmpresas
             // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(32, 484);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.RowTemplate.Height = 29;
-            dataGridView1.Size = new Size(755, 194);
-            dataGridView1.TabIndex = 54;
+            dataGridViewEmpresas.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewEmpresas.Location = new Point(32, 484);
+            dataGridViewEmpresas.Name = "dataGridViewEmpresas";
+            dataGridViewEmpresas.RowHeadersWidth = 51;
+            dataGridViewEmpresas.RowTemplate.Height = 29;
+            dataGridViewEmpresas.Size = new Size(755, 194);
+            dataGridViewEmpresas.TabIndex = 54;
             // 
-            // label8
+            // ListaDeEmpresas
             // 
-            label8.AutoSize = true;
-            label8.Font = new Font("Bahnschrift SemiBold", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            label8.Location = new Point(35, 445);
-            label8.Name = "label8";
-            label8.Size = new Size(177, 24);
-            label8.TabIndex = 53;
-            label8.Text = "Lista de Empresas";
+            ListaDeEmpresas.AutoSize = true;
+            ListaDeEmpresas.Font = new Font("Bahnschrift SemiBold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            ListaDeEmpresas.Location = new Point(35, 445);
+            ListaDeEmpresas.Name = "ListaDeEmpresas";
+            ListaDeEmpresas.Size = new Size(177, 24);
+            ListaDeEmpresas.TabIndex = 53;
+            ListaDeEmpresas.Text = "Lista de Empresas";
             // 
-            // textBox4
+            // txtSitioWeb
             // 
-            textBox4.Location = new Point(160, 327);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(156, 27);
-            textBox4.TabIndex = 50;
+            txtSitioWeb.Location = new Point(160, 327);
+            txtSitioWeb.Name = "txtSitioWeb";
+            txtSitioWeb.Size = new Size(156, 27);
+            txtSitioWeb.TabIndex = 50;
             // 
-            // textBox3
+            // txtRNC
             // 
-            textBox3.Location = new Point(160, 211);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(156, 27);
-            textBox3.TabIndex = 49;
+            txtRNC.Location = new Point(160, 211);
+            txtRNC.Name = "txtRNC";
+            txtRNC.Size = new Size(156, 27);
+            txtRNC.TabIndex = 49;
             // 
-            // textBox2
+            // txtNombre
             // 
-            textBox2.Location = new Point(160, 153);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(156, 27);
-            textBox2.TabIndex = 48;
+            txtNombre.Location = new Point(160, 153);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(156, 27);
+            txtNombre.TabIndex = 48;
             // 
-            // textBox1
+            // txtIDEmpresa
             // 
-            textBox1.Location = new Point(160, 98);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(53, 27);
-            textBox1.TabIndex = 47;
+            txtIDEmpresa.Location = new Point(160, 98);
+            txtIDEmpresa.Name = "txtIDEmpresa";
+            txtIDEmpresa.Size = new Size(53, 27);
+            txtIDEmpresa.TabIndex = 47;
             // 
-            // label7
+            // Direccion
             // 
-            label7.AutoSize = true;
-            label7.Font = new Font("Bahnschrift SemiBold", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            label7.Location = new Point(35, 383);
-            label7.Name = "label7";
-            label7.Size = new Size(95, 24);
-            label7.TabIndex = 46;
-            label7.Text = "Direccion";
+            Direccion.AutoSize = true;
+            Direccion.Font = new Font("Bahnschrift SemiBold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            Direccion.Location = new Point(35, 383);
+            Direccion.Name = "Direccion";
+            Direccion.Size = new Size(95, 24);
+            Direccion.TabIndex = 46;
+            Direccion.Text = "Direccion";
             // 
-            // label6
+            // SitioWeb
             // 
-            label6.AutoSize = true;
-            label6.Font = new Font("Bahnschrift SemiBold", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            label6.Location = new Point(35, 326);
-            label6.Name = "label6";
-            label6.Size = new Size(93, 24);
-            label6.TabIndex = 45;
-            label6.Text = "Sitio Web";
+            SitioWeb.AutoSize = true;
+            SitioWeb.Font = new Font("Bahnschrift SemiBold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            SitioWeb.Location = new Point(35, 326);
+            SitioWeb.Name = "SitioWeb";
+            SitioWeb.Size = new Size(93, 24);
+            SitioWeb.TabIndex = 45;
+            SitioWeb.Text = "Sitio Web";
             // 
-            // label5
+            // Propietario
             // 
-            label5.AutoSize = true;
-            label5.Font = new Font("Bahnschrift SemiBold", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            label5.Location = new Point(35, 266);
-            label5.Name = "label5";
-            label5.Size = new Size(112, 24);
-            label5.TabIndex = 44;
-            label5.Text = "Propietario";
+            Propietario.AutoSize = true;
+            Propietario.Font = new Font("Bahnschrift SemiBold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            Propietario.Location = new Point(35, 266);
+            Propietario.Name = "Propietario";
+            Propietario.Size = new Size(112, 24);
+            Propietario.TabIndex = 44;
+            Propietario.Text = "Propietario";
             // 
-            // label4
+            // RNC
             // 
-            label4.AutoSize = true;
-            label4.Font = new Font("Bahnschrift SemiBold", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            label4.Location = new Point(35, 210);
-            label4.Name = "label4";
-            label4.Size = new Size(49, 24);
-            label4.TabIndex = 43;
-            label4.Text = "RNC";
+            RNC.AutoSize = true;
+            RNC.Font = new Font("Bahnschrift SemiBold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            RNC.Location = new Point(35, 210);
+            RNC.Name = "RNC";
+            RNC.Size = new Size(49, 24);
+            RNC.TabIndex = 43;
+            RNC.Text = "RNC";
             // 
-            // label3
+            // Nombre
             // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Bahnschrift SemiBold", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            label3.Location = new Point(35, 153);
-            label3.Name = "label3";
-            label3.Size = new Size(83, 24);
-            label3.TabIndex = 42;
-            label3.Text = "Nombre";
+            Nombre.AutoSize = true;
+            Nombre.Font = new Font("Bahnschrift SemiBold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            Nombre.Location = new Point(35, 153);
+            Nombre.Name = "Nombre";
+            Nombre.Size = new Size(83, 24);
+            Nombre.TabIndex = 42;
+            Nombre.Text = "Nombre";
             // 
-            // label2
+            // IDEmpresa
             // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Bahnschrift SemiBold", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            label2.Location = new Point(35, 97);
-            label2.Name = "label2";
-            label2.Size = new Size(110, 24);
-            label2.TabIndex = 41;
-            label2.Text = "IDEmpresa";
-            label2.Click += label2_Click;
+            IDEmpresa.AutoSize = true;
+            IDEmpresa.Font = new Font("Bahnschrift SemiBold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            IDEmpresa.Location = new Point(35, 97);
+            IDEmpresa.Name = "IDEmpresa";
+            IDEmpresa.Size = new Size(110, 24);
+            IDEmpresa.TabIndex = 41;
+            IDEmpresa.Text = "IDEmpresa";
             // 
-            // label1
+            // GestionDeEmpresas
             // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Bahnschrift", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(275, 8);
-            label1.Name = "label1";
-            label1.Size = new Size(284, 34);
-            label1.TabIndex = 40;
-            label1.Text = "Gestion de Empresas";
+            GestionDeEmpresas.AutoSize = true;
+            GestionDeEmpresas.Font = new Font("Bahnschrift", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            GestionDeEmpresas.Location = new Point(275, 8);
+            GestionDeEmpresas.Name = "GestionDeEmpresas";
+            GestionDeEmpresas.Size = new Size(284, 34);
+            GestionDeEmpresas.TabIndex = 40;
+            GestionDeEmpresas.Text = "Gestion de Empresas";
             // 
-            // textBox5
+            // txtDireccion
             // 
-            textBox5.Location = new Point(160, 383);
-            textBox5.Name = "textBox5";
-            textBox5.Size = new Size(156, 27);
-            textBox5.TabIndex = 60;
+            txtDireccion.Location = new Point(160, 383);
+            txtDireccion.Name = "txtDireccion";
+            txtDireccion.Size = new Size(156, 27);
+            txtDireccion.TabIndex = 60;
             // 
-            // textBox6
+            // txtPropietario
             // 
-            textBox6.Location = new Point(160, 277);
-            textBox6.Name = "textBox6";
-            textBox6.Size = new Size(156, 27);
-            textBox6.TabIndex = 61;
+            txtPropietario.Location = new Point(160, 277);
+            txtPropietario.Name = "txtPropietario";
+            txtPropietario.Size = new Size(156, 27);
+            txtPropietario.TabIndex = 61;
             // 
             // FormGestionEmpresas
             // 
@@ -238,54 +237,55 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.Info;
             ClientSize = new Size(830, 690);
-            Controls.Add(textBox6);
-            Controls.Add(textBox5);
-            Controls.Add(button5);
-            Controls.Add(button4);
-            Controls.Add(button3);
-            Controls.Add(button2);
-            Controls.Add(button1);
-            Controls.Add(dataGridView1);
-            Controls.Add(label8);
-            Controls.Add(textBox4);
-            Controls.Add(textBox3);
-            Controls.Add(textBox2);
-            Controls.Add(textBox1);
-            Controls.Add(label7);
-            Controls.Add(label6);
-            Controls.Add(label5);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(label1);
+            Controls.Add(txtPropietario);
+            Controls.Add(txtDireccion);
+            Controls.Add(btnSalir);
+            Controls.Add(btnEliminar);
+            Controls.Add(btnActualizar);
+            Controls.Add(btnGuardar);
+            Controls.Add(btnNuevo);
+            Controls.Add(dataGridViewEmpresas);
+            Controls.Add(ListaDeEmpresas);
+            Controls.Add(txtSitioWeb);
+            Controls.Add(txtRNC);
+            Controls.Add(txtNombre);
+            Controls.Add(txtIDEmpresa);
+            Controls.Add(Direccion);
+            Controls.Add(SitioWeb);
+            Controls.Add(Propietario);
+            Controls.Add(RNC);
+            Controls.Add(Nombre);
+            Controls.Add(IDEmpresa);
+            Controls.Add(GestionDeEmpresas);
             Name = "FormGestionEmpresas";
             Text = "FormGestionEmpresas";
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            Load += FormGestionEmpresas_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridViewEmpresas).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private Button button5;
-        private Button button4;
-        private Button button3;
-        private Button button2;
-        private Button button1;
-        private DataGridView dataGridView1;
-        private Label label8;
-        private TextBox textBox4;
-        private TextBox textBox3;
-        private TextBox textBox2;
-        private TextBox textBox1;
-        private Label label7;
-        private Label label6;
-        private Label label5;
-        private Label label4;
-        private Label label3;
-        private Label label2;
-        private Label label1;
-        private TextBox textBox5;
-        private TextBox textBox6;
+        private Button btnSalir;
+        private Button btnEliminar;
+        private Button btnActualizar;
+        private Button btnGuardar;
+        private Button btnNuevo;
+        private DataGridView dataGridViewEmpresas;
+        private Label ListaDeEmpresas;
+        private TextBox txtSitioWeb;
+        private TextBox txtRNC;
+        private TextBox txtNombre;
+        private TextBox txtIDEmpresa;
+        private Label Direccion;
+        private Label SitioWeb;
+        private Label Propietario;
+        private Label RNC;
+        private Label Nombre;
+        private Label IDEmpresa;
+        private Label GestionDeEmpresas;
+        private TextBox txtDireccion;
+        private TextBox txtPropietario;
     }
 }
--- Sysnom2024/FormGestionUsuarios.cs
+++ Sysnom2024/FormGestionUsuarios.cs
@@ -7,29 +7,157 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
 
 namespace Sysnom2024
 {
-    public partial class FormGestionUsuarios : Form
-    {
-        public FormGestionUsuarios()
+        public partial class FormGestionUsuarios : Form
         {
-            InitializeComponent();
-        }
+            private readonly SqlConnection conexion = new SqlConnection("Integrated security=SSPI;data source=MSI\\SQLEXPRESS;persist security info=False;initial catalog=SysNom2024");
 
-        private void label2_Click(object sender, EventArgs e)
-        {
+            public FormGestionUsuarios()
+            {
+                InitializeComponent();
+            }
 
-        }
+            private void FormGestionUsuarios_Load(object sender, EventArgs e)
+            {
+                CargarComboBoxRoles();
+                CargarDataGridViewUsuario();
+            }
 
-        private void FormGestionUsuarios_Load(object sender, EventArgs e)
-        {
+            private void CargarComboBoxRoles()
+            {
+                using (SqlCommand cm = new SqlCommand("SP_CBRol", conexion))
+                {
+                    cm.CommandType = CommandType.StoredProcedure;
+                    SqlDataAdapter adapter = new SqlDataAdapter(cm);
+                    DataTable dt = new DataTable("Rol");
+                    adapter.Fill(dt);
+                    comboBoxRol.DataSource = dt;
+                    comboBoxRol.DisplayMember = "Nombre";
+                    comboBoxRol.ValueMember = "Rol";
+                }
+            }
 
-        }
+            private void CargarDataGridViewUsuario()
+            {
+            DataTable dataTable = new DataTable();
+            SqlCommand cm = new SqlCommand();
+            cm.Connection = conexion;
+            SqlDataAdapter adapter = new SqlDataAdapter("SP_DGVUsuario", conexion);
+            adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+            adapter.Fill(dataTable);
+            dataGridViewUsuario.DataSource = dataTable; ;
+            }
 
-        private void button5_Click(object sender, EventArgs e)
-        {
-            this.Close();
+            private void btnGuardar_Click(object sender, EventArgs e)
+            {
+                try
+                {
+                    using (SqlCommand cmd = new SqlCommand("SP_InsertaGestiondesuario", conexion))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@IDEmpleado", Convert.ToInt32(txtIDEmpleado.Text));
+                        cmd.Parameters.AddWithValue("@Nombre", txtNombre.Text);
+                        cmd.Parameters.AddWithValue("@UserName", txtUserName.Text);
+                        cmd.Parameters.AddWithValue("@Clave", txtClave.Text);
+                        cmd.Parameters.AddWithValue("@Rol", comboBoxRol.SelectedValue);
+
+                        conexion.Open();
+                        cmd.ExecuteNonQuery();
+                        conexion.Close();
+
+                        MessageBox.Show("Registro Guardado Satisfactoriamente");
+                        btnGuardar.Enabled = false;
+                        Limpiar();
+                        CargarDataGridViewUsuario();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ocurrió un error: " + ex.Message);
+                    if (conexion.State == ConnectionState.Open)
+                        conexion.Close();
+                }
+            }
+
+            private void btnActualizar_Click(object sender, EventArgs e)
+            {
+                try
+                {
+                    using (SqlCommand cmd = new SqlCommand("SP_ActualizaGestiondeusuario", conexion))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@IDEmpleado", Convert.ToInt32(txtIDEmpleado.Text));
+                        cmd.Parameters.AddWithValue("@Nombre", txtNombre.Text);
+                        cmd.Parameters.AddWithValue("@UserName", txtUserName.Text);
+                        cmd.Parameters.AddWithValue("@Clave", txtClave.Text);
+                        cmd.Parameters.AddWithValue("@Rol", comboBoxRol.SelectedValue);
+
+                        conexion.Open();
+                        cmd.ExecuteNonQuery();
+                        conexion.Close();
+
+                        MessageBox.Show("Registro Actualizado Satisfactoriamente");
+                        Limpiar();
+                        CargarDataGridViewUsuario();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ocurrió un error: " + ex.Message);
+                    if (conexion.State == ConnectionState.Open)
+                        conexion.Close();
+                }
+            }
+
+            private void btnEliminar_Click(object sender, EventArgs e)
+            {
+                try
+                {
+                    using (SqlCommand cmd = new SqlCommand("SP_EliminaGestiondeusuario", conexion))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@IDEmpleado", Convert.ToInt32(txtIDEmpleado.Text));
+
+                        conexion.Open();
+                        cmd.ExecuteNonQuery();
+                        conexion.Close();
+
+                        MessageBox.Show("Registro Eliminado Satisfactoriamente");
+                        Limpiar();
+                        CargarDataGridViewUsuario();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ocurrió un error: " + ex.Message);
+                    if (conexion.State == ConnectionState.Open)
+                        conexion.Close();
+                }
+            }
+
+            private void btnNuevo_Click(object sender, EventArgs e)
+            {
+                Limpiar();
+                btnGuardar.Enabled = true;
+            }
+
+            private void btnSalir_Click(object sender, EventArgs e)
+            {
+                this.Close();
+            }
+
+            private void Limpiar()
+            {
+                txtIDEmpleado.Text = "";
+                txtNombre.Text = "";
+                txtUserName.Text = "";
+                txtClave.Text = "";
+                comboBoxRol.SelectedIndex = -1;
+            }
         }
     }
-}
